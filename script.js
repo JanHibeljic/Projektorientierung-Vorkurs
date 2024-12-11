2c2f8b3b9e4a6cf39877ef7475b816832928d51a
Jan Hibeljic <j.hibeljic@gmx.de>
2024-12-11 16:31:45 +0100

    added class, myContact adds into JSON- not optimized

--- script.js
+++ script.js
@@ -10,16 +10,22 @@ let contacts = [
 // Beschreibung, wie ein Objekt aussehen soll
 // Vorlage
 // Schablone
-class Contacts {
+class Contact {
   firstName;
   lastName;
 }
 
-function addContact(firstName, lastName) {
-  contacts.push({
-    firstName: firstName,
-    lastName: lastName,
-  });
+//new Contact();
+//{
+//  firstName: undefined,
+//  lastName: undefined,
+//}
+function addContact(fN, lN) {
+  let myContact = new Contact();
+  myContact["firstName"] = fN;
+  myContact["firstName"] = lN;
+  contacts.push(myContact);
+  debugger;
 }
 
 addContact("Jan", "Hibeljic");
